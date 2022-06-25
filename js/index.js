e6bac10e95048a2fb358e04ca023f4d81e5d8958
Misvetozar <misvetozar@gmail.com>
2022-06-25 15:25:08 +0300

    first_commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const username = prompt('Please enter your name')
+
+const usersurname = prompt('Please enter your surname')
+
+const userage= prompt('Please enter your age')
+
+const userhobby= prompt('Please enter your hobby')
+
+console.log(username, 'user name')
+console.log(usersurname, 'user surname')
+console.log(userage, 'user age')
+
+alert('Hi ' + username +  ' ' + usersurname)
